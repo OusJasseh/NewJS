7db12129b28357680b7ef23f2dddda92e5994b9c
OusJasseh <ousman16@hotmail.com>
2022-05-21 17:50:40 +0200

    Events

--- Events/events.js
+++ Events/events.js
@@ -1,28 +1,45 @@
-import { validateMinLength } from "./validatescript.js";
+import { validateEmail, validateMinLength, validatePassword } from "./validatescript.js";
 
 document.getElementById("name").addEventListener("keyup", function(event) {
     validateMinLength(event.target)
 })
 
+document.getElementById("email").addEventListener("keyup", function (event) {
+        validateEmail(event.target)
+});
+
+document.getElementById("password").addEventListener("keyup", function (event) {
+    validatePassword(event.target, 8);
+});
+
+document.getElementById("message").addEventListener("keyup", function (event) {
+    validateMinLength(event.target, 5);
+});
+
+
+
 document.getElementById("ContactForm").addEventListener("submit", function(event) {
     event.preventDefault()
 
-   
 
     for (let element of event.target)
      
         
-        switch (element.tagName) {
-          
-            case "INPUT":
-                validateMinLength(element)
-                break;
+        switch (element.type) {
+          case "text":
+            validateMinLength(element);
+            break;
 
-            case "SELECT":
+          case "email":
+            validateEmail(element);
+            break;
 
-                break;
+          case "password":
+            validatePassword(element);
+            break;
 
-            case "TEXTAREA":
-                break;
+          case "textarea":
+              validateMinLength(element, 5)
+            break;
         }
 })
--- Events/validatescript.js
+++ Events/validatescript.js
@@ -1,5 +1,39 @@
 export function validateMinLength(element, minLength = 2) {
-    if (element.value.length < minLength) 
-        document.getElementById(`error-${element.id}`).innerText = "Hej"
+    if (element.value.length < minLength) {
+        document.getElementById(`error-${element.id}`).innerText = `Måste innehålla minst ${minLength} tecken. `
+        return false
+    }
+    
+    else {
+            document.getElementById(`error-${element.id}`).innerText = "";
+            return true
+    }
+        
+}
 
+export function validateEmail(element) {
+    //regular expressions (regEx)
+    const regEx =    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    if (!regEx.test(element.value)) {
+         document.getElementById(`error-${element.id}`).innerText = `Måste vara en giltig e-postadress.`;
+         return false
+    } else{
+        document.getElementById(`error-${element.id}`).innerText = ` `;
+        return true
+          }
+    
+}
+
+export function validatePassword(element){
+    const regEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+
+    if (!regEx.test(element.value)){
+              document.getElementById(`error-${element.id}`).innerText = `Måste vara stark lösenord.`;
+              return false
+    } else {
+            document.getElementById(`error-${element.id}`).innerText = ` `;
+            return true
+    }
+        
 }
